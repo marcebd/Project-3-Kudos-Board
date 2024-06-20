89ee10055841780a091771edf28ffa09bcc86020
Marcela Billingslea Durini <marcebd@meta.com>
2024-06-20 15:19:51 -0700

    added components files

---
+++ FRONTEND/Kudos-Board/src/JSX-Components/BoardContainer.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react';
+
+
+function BoardContainer() {
+
+    return(
+        <>
+
+        </>
+    )
+
+}
+
+export default BoardContainer;
---
+++ FRONTEND/Kudos-Board/src/JSX-Components/CardModal.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react';
+
+
+function CardModal() {
+
+    return(
+        <>
+
+        </>
+    )
+
+}
+
+export default CardModal;
