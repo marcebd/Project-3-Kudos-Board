6687386535cdb293843e16cd7a640ad735e7b927
Marcela Billingslea Durini <marcebd@meta.com>
2024-06-20 11:29:43 -0700

    basic data base functions

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -9,11 +9,65 @@ const prisma = new PrismaClient()
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
   })
-
-// Return all the photo objects
-app.get('/photos', async (req, res) => {
-    const photos = await prisma.photo.findMany();
-    res.status(200).json(photos);
+/*********************** BOARDS ***********************/
+//Return all the boards objects
+app.get('/boards', async (req, res) => {
+    const board = await prisma.board.findMany();
+    res.status(200).json(board);
 });
 
+//Create a new board
+app.post('/boards', async (req, res) => {
+    const {imgUrl, title, category} = req.body;
+    const newBoard = await prisma.board.create({
+        data: {
+            imgUrl,
+            title,
+            category
+        }
+    })
+    res.status(201).json(newBoard);
+});
 
+//Get specific board
+app.get('/boards/:id', async (req, res) => {
+    const {id} = req.params;
+    const board = await prisma.board.findUnique(
+        {
+            where: { id: parseInt(id) }
+        });
+    res.status(200).json(board);
+});
+
+/*********************** CARDS ***********************/
+
+// Return all the cards objects
+app.get('/cards', async (req, res) => {
+    const card = await prisma.card.findMany();
+    res.status(200).json(cards);
+});
+
+//Create a new card
+app.post('/cards', async (req, res) => {
+    const {creator, title, message, GIFUrl, signature} = req.body;
+    const newCard = await prisma.card.create({
+        data: {
+            creator,
+            title,
+            message,
+            GIFUrl,
+            signature
+        }
+    })
+    res.status(201).json(newCard);
+});
+
+//Get specific card
+app.get('/cards/:id', async (req, res) => {
+    const {id} = req.params;
+    const card = await prisma.card.findUnique(
+        {
+            where: { id: parseInt(id) }
+        });
+    res.status(200).json(card);
+});
