66502656aac823e3906416e45fb5e013b8bdbc71
Marcela Billingslea Durini <marcebd@meta.com>
2024-06-20 17:22:41 -0700

    cors added

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
-
+const cors = require('cors')
 const PORT = process.env.PORT || 3000
 
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
   })
